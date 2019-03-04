b760dfcf83d02cf6e2090867bacc2549d05a6083
Mark <mgordon52192@gmail.com>
2019-03-04 04:25:19 -0500

    MacOS Build?

--- squirrel.packager.js
+++ squirrel.packager.js
@@ -1,12 +1,27 @@
-const ei = require('electron-winstaller');
 
 
-const result = ei.createWindowsInstaller({
-    appDirectory: `blink-${process.platform}-${process.arch}/`,
-    authors: 'cairthenn',
-    setupExe: `setup-${process.platform}-${process.arch}.exe`,
-    noMsi: true,
-});
+const dir = `blink-${process.platform}-${process.arch}/`;
+const author = 'cairthenn';
 
-result.then(() => console.log('Installer created successfully'))
-    .catch(err => console.log(`Error creating installer: ${err}`));
+if(process.platform === 'win32') {
+    const ei = require('electron-winstaller');
+    const result = ei.createWindowsInstaller({
+        appDirectory: dir,
+        authors: author,
+        setupExe: `setup-${process.platform}-${process.arch}.exe`,
+        noMsi: true,
+    });
+    
+    result.then(() => console.log('Installer created successfully'))
+        .catch(err => console.log(`Error creating installer: ${err}`));    
+} else if(process.platform === 'darwin') {
+    const dmg = require('electron-installer-dmg')
+    dmg({
+        appPath: dir,
+        name: 'Blink',
+        path: 'installer',
+    }, (err) => {
+        console.log(err);
+    })
+}
+
